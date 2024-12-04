b330672b0d4ff5125bf3a118a16240bce5bce774
kawadumax <kazmax7@gmail.com>
2024-12-04 16:08:36 +0900

    Add Axios

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tasks', TaskApiController::class);
+    Route::apiResource('tasks', TaskApiController::class, ['as' => 'api']);
 });
