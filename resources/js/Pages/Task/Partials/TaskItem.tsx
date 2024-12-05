224daa3ad31bd0ce0c5c20f42eb46c2041f5a7a9
kawadumax <kazmax7@gmail.com>
2024-12-05 14:46:32 +0900

    Enable Deletion of task

--- resources/js/Pages/Task/Partials/TaskItem.tsx
+++ resources/js/Pages/Task/Partials/TaskItem.tsx
@@ -2,7 +2,37 @@ import { Task } from "@/types";
 import { Button } from "@/Components/ui/button";
 import { Checkbox } from "@/Components/ui/checkbox";
 import { Trash, Edit } from "@mynaui/icons-react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { tasksAtom } from "@/Lib/atoms";
 export const TaskItem = ({ task }: { task: Task }) => {
+    const [, setTasks] = useAtom(tasksAtom);
+
+    const handleCheckboxChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        //TODO
+    };
+
+    const handleDelete = () => {
+        axios
+            .delete(route("api.tasks.destroy", task.id))
+            .then((response) => {
+                setTasks((prev) => prev.filter((t) => t.id !== task.id));
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {});
+    };
+
+    const handleEdit = () => {
+        //TODO
+    };
+
+    useEffect(() => {}, []);
+
     return (
         <li className="px-4 list-none hover:bg-stone-100">
             <div className="flex flex-row flex-nowrap justify-between">
@@ -15,7 +45,11 @@ export const TaskItem = ({ task }: { task: Task }) => {
                     <Button variant={"ghost"} size={"icon"}>
                         <Edit />
                     </Button>
-                    <Button variant={"ghost"} size={"icon"}>
+                    <Button
+                        variant={"ghost"}
+                        size={"icon"}
+                        onClick={handleDelete}
+                    >
                         <Trash />
                     </Button>
                 </span>
