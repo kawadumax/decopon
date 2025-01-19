ec7f210a85bfd14e43c2395364e6b3ef989956a0
kawadumax <kazmax7@gmail.com>
2025-01-19 17:17:00 +0900

    Get Logs by TaskID

--- resources/js/Pages/Task/Partials/TaskLogger.tsx
+++ resources/js/Pages/Task/Partials/TaskLogger.tsx
@@ -1,11 +1,17 @@
 import { Input } from "@/Components/ui/input";
-import { Task } from "@/types";
+import { useApi } from "@/Hooks/useApi";
+import { Task, Log } from "@/types";
+import { useAtom, useAtomValue } from "jotai";
 import { PrimitiveAtom } from "jotai";
 import React, { useEffect, useRef, useState } from "react";
 
 export const TaskLogger = ({ taskAtom }: { taskAtom: PrimitiveAtom<Task> }) => {
+    const api = useApi();
+    const task = useAtomValue(taskAtom);
     const [logs, setLogs] = useState(["aaa", "bbb", "ccc", "ddd"]);
+
     const logContainerRef = useRef<HTMLUListElement>(null);
+
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key !== "Enter") return;
         setLogs((prev) => [...prev, "uho"]);
@@ -17,10 +23,27 @@ export const TaskLogger = ({ taskAtom }: { taskAtom: PrimitiveAtom<Task> }) => {
                 logContainerRef.current.scrollHeight;
         }
     }, [logs]);
+
+    useEffect(() => {
+        console.log("log effect")
+        if (task) {
+            api.get(
+                route("api.logs.task", task.id),
+                (response) => {
+                    const logs = response.data.map((log : Log) => log.content);
+                    setLogs(logs);
+                },
+                (error) => {
+                    console.error("Error fetching logs:", error);
+                }
+            );
+        }
+    }, [task]);
+
     return (
         <>
             <ul ref={logContainerRef} className="max-h-80 overflow-y-auto">
-                {logs.map((log, index) => (
+                {logs && logs.map((log, index) => (
                     <li key={index}>{log}</li>
                 ))}
             </ul>
