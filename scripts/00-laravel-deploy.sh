8357471761af06edd75382fd20fb9a92f90c7fcd
kawadumax <kazmax7@gmail.com>
2025-01-22 16:04:30 +0900

    Update for deploy to render

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+echo "Caching config..."
+php artisan config:cache
+echo "Caching routes..."
+php artisan route:cache
+echo "Running npm install..."
+npm install
+echo "Running npm build..."
+npm run build
+echo "Running migrations..."
+php artisan migrate --force
