455d755e3dfce6b5cda64ddbe085d03dc8b49122
kawadumax <kazmax7@gmail.com>
2025-01-31 14:42:56 +0900

    Add Pane

--- resources/js/Pages/Task/Index.tsx
+++ resources/js/Pages/Task/Index.tsx
@@ -8,6 +8,7 @@ import { useAtom } from "jotai";
 import { tasksAtom } from "@/Lib/atoms";
 import { useEffect } from "react";
 import Split from "react-split";
+import { Timer } from "@/Components/Timer";
 
 export default function Index(
     props: PageProps<{
@@ -25,30 +26,45 @@ export default function Index(
             <Head title="Task List" />
 
             <Split
-                className="flex flex-row min-h-full max-h-full"
+                className="flex flex-row min-h-full max-h-full bg-white"
                 sizes={[17.2, 41.4, 41.4]} // 白銀比を元にした比率
-                gutterSize={4}
+                gutterSize={2}
                 gutter={() => {
                     const gutterElement = document.createElement("div");
-                    gutterElement.className = `w-1 bg-amber-400 hover:cursor-col-resize hover:w-2 hover:bg-amber-500 transition-all delay-300 duration-300 ease-in-out`;
+                    gutterElement.className = `w-1 bg-amber-50/50 hover:cursor-col-resize hover:w-2 hover:bg-amber-400 transition-all delay-100 duration-300 ease-in-out`;
                     return gutterElement;
                 }}
                 // 元のgutterのスタイルを削除
                 gutterStyle={() => ({})}
             >
-                <div className="overflow-auto bg-white shadow-sm dark:bg-gray-800 max-h-full">
-                    <ul>
-                        <li>#daily</li>
-                        <li>#hoge</li>
-                        <li>#foo</li>
-                        <li>#bar</li>
-                    </ul>
-                </div>
+                <Split
+                    className="split flex flex-col"
+                    sizes={[50, 50]}
+                    gutterSize={4}
+                    gutter={() => {
+                        const gutterElement = document.createElement("div");
+                        gutterElement.className = `h-1 bg-amber-50/50 hover:cursor-row-resize hover:h-2 hover:bg-amber-400 transition-all delay-100 duration-300 ease-in-out`;
+                        return gutterElement;
+                    }}
+                    direction="vertical"
+                >
+                    <div className="bg-stone-50/50 shadow-sm dark:bg-gray-800">
+                        <ul>
+                            <li>#daily</li>
+                            <li>#hoge</li>
+                            <li>#foo</li>
+                            <li>#bar</li>
+                        </ul>
+                    </div>
+                    <div className="bg-stone-50/50 shadow-sm dark:bg-gray-800 max-h-full">
+                        <Timer></Timer>
+                    </div>
+                </Split>
                 <div className="overflow-auto hidden-scrollbar bg-white shadow-sm dark:bg-gray-800 max-h-full">
                     <TaskTools></TaskTools>
                     <TaskTree></TaskTree>
                 </div>
-                <div className="overflow-hidden bg-white shadow-sm dark:bg-gray-800 max-h-full">
+                <div className="overflow-hidden bg-stone-50/50 shadow-sm dark:bg-gray-800 max-h-full">
                     <TaskSideView></TaskSideView>
                 </div>
             </Split>
