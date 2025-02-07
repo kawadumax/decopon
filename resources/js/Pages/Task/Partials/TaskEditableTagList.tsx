85bf9ff209952adbaff3ab205d4aec151b3ee94e
kawadumax <kazmax7@gmail.com>
2025-02-07 13:35:43 +0900

    Add TagInput

--- resources/js/Pages/Task/Partials/TaskEditableTagList.tsx
+++ resources/js/Pages/Task/Partials/TaskEditableTagList.tsx
@@ -1,3 +1,30 @@
+import { Tag, TagInput } from "emblor";
+import { useState } from "react";
+
 export const TaskEditableTagList = () => {
-    return <input type="text" />;
+    const [tags, setTags] = useState<Tag[]>([]);
+    const [activeTagIndex, setActiveTagIndex] = useState<number | null>(null);
+
+    return (
+        <TagInput
+            placeholder="Add a tag"
+            tags={tags}
+            styleClasses={{
+                input: "w-full border-solid border-b-1 shadow-none focus-visible:ring-0",
+                tag: {
+                    body: "h-6 p-2",
+                    closeButton: "p-1 pr-0 hover:text-primary-foreground",
+                },
+                inlineTagsContainer: "border-0 px-0",
+            }}
+            setTags={(newTags) => {
+                setTags(newTags);
+            }}
+            activeTagIndex={activeTagIndex}
+            setActiveTagIndex={setActiveTagIndex}
+            size={"sm"}
+            shape={"pill"}
+            variant={"primary"}
+        />
+    );
 };
