5ac16bf5bfb11d4a6c5469939e5d8ce8d41ecffa
kawadumax <kazmax7@gmail.com>
2025-02-10 16:30:22 +0900

    Wip Timeline

--- resources/js/Pages/Task/Partials/TaskLogger.tsx
+++ resources/js/Pages/Task/Partials/TaskLogger.tsx
@@ -1,39 +1,14 @@
-import { Input } from "@/Components/ui/input";
+import { LogItem } from "@/Components/LogItem";
 import { useApi } from "@/Hooks/useApi";
 import { logger } from "@/Lib/logger";
 import { Task, Log } from "@/types";
-import { useAtom, useAtomValue } from "jotai";
+import { useAtomValue } from "jotai";
 import { PrimitiveAtom } from "jotai";
 import React, { useEffect, useRef, useState } from "react";
 import {
     AutosizeTextarea,
     AutosizeTextAreaRef,
 } from "@/Components/ui/autosize-textarea";
-
-const formatDate = (isoString: string): string => {
-    const date = new Date(isoString);
-    return new Intl.DateTimeFormat("ja-JP", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-    })
-        .format(date)
-        .replace(/\//g, "/");
-};
-
-const LogItem = ({ log }: { log: Log }) => {
-    return (
-        <li className="rounded flex flex-row justify-between hover:ring-1 hover:ring-amber-400 m-1 p-1">
-            <p className="text-base">{log.content}</p>
-            <p className="font-mono text-xs text-black text-opacity-50">
-                {formatDate(log.created_at)}
-            </p>
-        </li>
-    );
-};
 
 export const TaskLogger = ({ taskAtom }: { taskAtom: PrimitiveAtom<Task> }) => {
     const api = useApi();
