c18e6d37c54f46dc9b408e9ca4394f91356c84b5
kawadumax <kazmax7@gmail.com>
2025-02-11 13:06:48 +0900

    Add tagList to timeline page

--- resources/js/Pages/Task/Partials/TaskTagList.tsx
+++ resources/js/Pages/Task/Partials/TaskTagList.tsx
@@ -1,38 +1,6 @@
-import { useApi } from "@/Hooks/useApi";
-import { Tag } from "@/types";
-import { useEffect, useState } from "react";
-
+import { TagList } from "@/Components/TagList";
 export const TaskTagList = () => {
-    const [tags, setTags] = useState<Tag[]>([]);
-    const api = useApi();
-    useEffect(() => {
-        // Initialize Tags
-        api.get(route("api.tags.index"), (response) => {
-            setTags(response.data.tags);
-        });
-    }, []);
     return (
-        <>
-            <h3 className="font-bold text-base sticky border-primary border-b-2 p-2 top-0">
-                Latest Tags
-            </h3>
-            <ul className="text-primary font-bold">
-                {tags
-                    ? tags.map((tag, index) => {
-                          return (
-                              <li
-                                  key={index}
-                                  className="px-2 cursor-pointer hover:bg-stone-100"
-                              >
-                                  <span className="px-1 font-thin rounded border-1 border-primary bg-stone-100 mr-2">
-                                      #
-                                  </span>
-                                  {tag.name}
-                              </li>
-                          );
-                      })
-                    : "No Tags There"}
-            </ul>
-        </>
-    );
+        <TagList></TagList>
+    )
 };
