987e271b0b64b95dad05c0e9389b260319fcfcd9
kawadumax <kazmax7@gmail.com>
2025-02-14 22:55:10 +0900

    Enable Checkbox on Tag Table

--- resources/js/Pages/Tag/Partials/TagTableRow.tsx
+++ resources/js/Pages/Tag/Partials/TagTableRow.tsx
@@ -1,33 +1,34 @@
-import { Tag } from "@/types";
-import { TableCell, TableRow } from "../../../Components/ui/table";
-import { Checkbox } from "../../../Components/ui/checkbox";
 import { formatDate } from "@/Lib/formatDate";
-import { CheckedState } from "@radix-ui/react-checkbox";
+import type { Tag, TagCheckable } from "@/types";
+import type { CheckedState } from "@radix-ui/react-checkbox";
+import { useEffect } from "react";
+import { Checkbox } from "../../../Components/ui/checkbox";
+import { TableCell, TableRow } from "../../../Components/ui/table";
 export const TagTableRow = ({
-    tag,
-    onClick,
-    onCheckedChange,
+	tag,
+	onClick,
+	onCheckedChange,
 }: {
-    tag: Tag;
-    onClick: React.MouseEventHandler<HTMLTableRowElement>;
-    onCheckedChange: (checked: CheckedState) => void;
+	tag: TagCheckable;
+	onClick: React.MouseEventHandler<HTMLTableRowElement>;
+	onCheckedChange: (checked: CheckedState) => void;
 }) => {
-    return (
-        <TableRow
-            className="px-2 cursor-pointer hover:bg-stone-100"
-            onClick={onClick}
-        >
-            <TableCell>
-                <Checkbox onCheckedChange={onCheckedChange}></Checkbox>
-            </TableCell>
-            <TableCell className="text-primary">
-                <span className="px-1 font-thin rounded border-1 border-primary bg-stone-100 mr-2">
-                    #
-                </span>
-                {tag.name}
-            </TableCell>
-            <TableCell>{formatDate(tag.created_at)}</TableCell>
-            <TableCell>{formatDate(tag.updated_at)}</TableCell>
-        </TableRow>
-    );
+	return (
+		<TableRow
+			className="px-2 cursor-pointer hover:bg-stone-100"
+			onClick={onClick}
+		>
+			<TableCell>
+				<Checkbox onCheckedChange={onCheckedChange} checked={tag.checked} />
+			</TableCell>
+			<TableCell className="text-primary">
+				<span className="px-1 font-thin rounded border-1 border-primary bg-stone-100 mr-2">
+					#
+				</span>
+				{tag.name}
+			</TableCell>
+			<TableCell>{formatDate(tag.created_at)}</TableCell>
+			<TableCell>{formatDate(tag.updated_at)}</TableCell>
+		</TableRow>
+	);
 };
