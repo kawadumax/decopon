ff454b428bb8f8e1e7e11dd566535034e677f536
kawadumax <kazmax7@gmail.com>
2025-02-17 13:39:24 +0900

    Eneble reset current tag when tag deleted

--- resources/js/Pages/Task/Partials/TaskEditableTagList.tsx
+++ resources/js/Pages/Task/Partials/TaskEditableTagList.tsx
@@ -1,107 +1,95 @@
 import { useApi } from "@/Hooks/useApi";
-import { Task } from "@/types";
-import { Tag as EmblorTag, TagInput } from "emblor";
-import { Tag } from "@/types";
+import { toEmblorTags } from "@/Lib/utils";
+import type { Task } from "@/types";
+import type { Tag } from "@/types";
+import { type Tag as EmblorTag, TagInput } from "emblor";
 import { useAtom, useAtomValue } from "jotai";
+import type { PrimitiveAtom } from "jotai";
 import { useCallback, useEffect, useState } from "react";
-import { PrimitiveAtom } from "jotai";
 
 export const TaskEditableTagList = ({
-    taskAtom,
+	taskAtom,
 }: {
-    taskAtom: PrimitiveAtom<Task>;
+	taskAtom: PrimitiveAtom<Task>;
 }) => {
-    const toEmblorTags = (tags: Tag[]) => {
-        if (tags?.length) {
-            return tags.map((tag) => {
-                return { id: `${tag.id}`, text: tag.name };
-            });
-        } else {
-            return [];
-        }
-    };
-    const [activeTagIndex, setActiveTagIndex] = useState<number | null>(null);
-    const [currentTask, setCurrentTask] = useAtom(taskAtom);
-    const [tags, setTags] = useState<EmblorTag[]>(
-        toEmblorTags(currentTask.tags)
-    );
-    const api = useApi();
+	const [activeTagIndex, setActiveTagIndex] = useState<number | null>(null);
+	const [currentTask, setCurrentTask] = useAtom(taskAtom);
+	const [tags, setTags] = useState<EmblorTag[]>(toEmblorTags(currentTask.tags));
+	const api = useApi();
 
-    const handleTagAdded = useCallback(
-        (tagText: string) => {
-            console.log("tag added", tagText);
-            api.post(
-                route("api.tags.relation.post"),
-                {
-                    task_id: currentTask.id,
-                    name: tagText,
-                },
-                (response) => {
-                    console.log(response);
-                    setCurrentTask((prev) => {
-                        return {
-                            ...prev,
-                            tags: [...prev.tags, response.data.tag],
-                        };
-                    });
-                }
-            );
-        },
-        [api, currentTask]
-    );
+	const handleTagAdded = useCallback(
+		(tagText: string) => {
+			console.log("tag added", tagText);
+			api.post(
+				route("api.tags.relation.post"),
+				{
+					task_id: currentTask.id,
+					name: tagText,
+				},
+				(response) => {
+					console.log(response);
+					setCurrentTask((prev) => {
+						return {
+							...prev,
+							tags: [...prev.tags, response.data.tag],
+						};
+					});
+				},
+			);
+		},
+		[api, currentTask, setCurrentTask],
+	);
 
-    const handleTagRemoved = useCallback(
-        (tagText: string) => {
-            console.log("tag removed", tagText, currentTask);
-            api.delete(
-                route("api.tags.relation.destroy"),
-                {
-                    task_id: currentTask.id,
-                    name: tagText,
-                },
-                (response) => {
-                    console.log(response);
-                    setCurrentTask((prev) => {
-                        const newTags = prev.tags
-                            ? prev.tags.filter(
-                                  (tag) => tag.name == response.data.tag.name
-                              )
-                            : [];
-                        return {
-                            ...prev,
-                            tags: [...newTags],
-                        };
-                    });
-                }
-            );
-        },
-        [currentTask, api]
-    );
+	const handleTagRemoved = useCallback(
+		(tagText: string) => {
+			console.log("tag removed", tagText, currentTask);
+			api.delete(
+				route("api.tags.relation.destroy"),
+				{
+					task_id: currentTask.id,
+					name: tagText,
+				},
+				(response) => {
+					console.log(response);
+					setCurrentTask((prev) => {
+						const newTags = prev.tags
+							? prev.tags.filter((tag) => tag.name === response.data.tag.name)
+							: [];
+						return {
+							...prev,
+							tags: [...newTags],
+						};
+					});
+				},
+			);
+		},
+		[currentTask, api, setCurrentTask],
+	);
 
-    useEffect(() => {
-        setTags(toEmblorTags(currentTask.tags));
-    }, [currentTask]);
+	useEffect(() => {
+		setTags(toEmblorTags(currentTask.tags));
+	}, [currentTask]);
 
-    return (
-        <TagInput
-            placeholder="Add a tag"
-            styleClasses={{
-                input: "w-full shadow-none",
-                tag: {
-                    body: "h-6 p-2",
-                    closeButton: "p-1 pr-0 hover:text-primary-foreground",
-                },
-                inlineTagsContainer: "border-0 px-0",
-            }}
-            tags={tags}
-            setTags={setTags}
-            activeTagIndex={activeTagIndex}
-            setActiveTagIndex={setActiveTagIndex}
-            onTagAdd={handleTagAdded}
-            onTagRemove={handleTagRemoved}
-            size={"sm"}
-            shape={"pill"}
-            variant={"primary"}
-        />
-    );
+	return (
+		<TagInput
+			placeholder="Add a tag"
+			styleClasses={{
+				input: "w-full shadow-none",
+				tag: {
+					body: "h-6 p-2",
+					closeButton: "p-1 pr-0 hover:text-primary-foreground",
+				},
+				inlineTagsContainer: "border-0 px-0",
+			}}
+			tags={tags}
+			setTags={setTags}
+			activeTagIndex={activeTagIndex}
+			setActiveTagIndex={setActiveTagIndex}
+			onTagAdd={handleTagAdded}
+			onTagRemove={handleTagRemoved}
+			size={"sm"}
+			shape={"pill"}
+			variant={"primary"}
+		/>
+	);
 };
