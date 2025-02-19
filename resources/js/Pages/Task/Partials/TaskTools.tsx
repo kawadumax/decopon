fdec0ab3984e106a5f676f3015931c4da661b31d
kawadumax <kazmax7@gmail.com>
2025-02-19 11:12:29 +0900

    Enable: Task created with title

--- resources/js/Pages/Task/Partials/TaskTools.tsx
+++ resources/js/Pages/Task/Partials/TaskTools.tsx
@@ -1,31 +1,36 @@
-import { Button } from "@/Components/ui/button";
+import AddItemInput from "@/Components/AddItemInput";
+import { useApi } from "@/Hooks/useApi";
 import { tasksAtom } from "@/Lib/atoms";
 import { useAtom } from "jotai";
-import { Plus } from "@mynaui/icons-react";
-import { useApi } from "@/Hooks/useApi";
+import { useCallback } from "react";
 
 export const TaskTools = () => {
-    const [, setTasks] = useAtom(tasksAtom);
-    const api = useApi();
+	const [, setTasks] = useAtom(tasksAtom);
+	const api = useApi();
 
-    const handleAddNewTask = () => {
-        const taskTemplate = {
-            title: "New Task",
-            description: "New Task Description",
-            completed: false,
-            parent_task_id: null,
-        };
+	const handleAddNewTask = useCallback(
+		(title: string) => {
+			const taskTemplate = {
+				title,
+				description: "New Task Description",
+				completed: false,
+				parent_task_id: null,
+			};
 
-        api.post(route("api.tasks.store"), taskTemplate, (response) => {
-            setTasks((prev) => [...prev, response.data.task]);
-        });
-    };
+			api.post(route("api.tasks.store"), taskTemplate, (response) => {
+				setTasks((prev) => [...prev, response.data.task]);
+			});
+		},
+		[api, setTasks],
+	);
 
-    return (
-        <div className="flex justify-start m-4 mb-0">
-            <Button onClick={handleAddNewTask}>
-                <Plus /> Add New Task
-            </Button>
-        </div>
-    );
+	return (
+		<div className="flex justify-start m-4 mb-0">
+			<AddItemInput
+				placeholder="Add Task Title"
+				buttonText="Add"
+				onAddItem={handleAddNewTask}
+			/>
+		</div>
+	);
 };
