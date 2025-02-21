5e9a71c35e98e8013b4743f0071172bf0484b507
kawadumax <kazmax7@gmail.com>
2025-02-22 00:28:33 +0900

    i18n: Profile

--- resources/js/Pages/Profile/Partials/UpdatePreferenceForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdatePreferenceForm.tsx
@@ -3,94 +3,103 @@ import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
 import { Transition } from "@headlessui/react";
-import { Link, useForm, usePage } from "@inertiajs/react";
-import { FormEventHandler } from "react";
+import { useForm, usePage } from "@inertiajs/react";
+import type { FormEventHandler } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function UpdatePreferenceForm({
-    className = "",
+	className = "",
 }: {
-    className?: string;
+	className?: string;
 }) {
-    const user = usePage().props.auth.user;
+	const { t } = useTranslation();
+	const user = usePage().props.auth.user;
 
-    const { data, setData, patch, errors, processing, recentlySuccessful } =
-        useForm({
-            work_time: user.preference?.work_time || 25,
-            break_time: user.preference?.break_time || 5,
-        });
+	const { data, setData, patch, errors, processing, recentlySuccessful } =
+		useForm({
+			work_time: user.preference?.work_time || 25,
+			break_time: user.preference?.break_time || 5,
+		});
 
-    const submit: FormEventHandler = (e) => {
-        e.preventDefault();
+	const submit: FormEventHandler = (e) => {
+		e.preventDefault();
 
-        patch(route("preference.update"));
-    };
+		patch(route("preference.update"));
+	};
 
-    return (
-        <section className={className}>
-            <header>
-                <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">
-                    Preference Information
-                </h2>
+	return (
+		<section className={className}>
+			<header>
+				<h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">
+					{t("profile.updatePreference.title")}
+				</h2>
 
-                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Update your account's preference information and email
-                    address.
-                </p>
-            </header>
+				<p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+					{t("profile.updatePreference.description")}
+				</p>
+			</header>
 
-            <form onSubmit={submit} className="mt-6 space-y-6">
-                <div>
-                    <InputLabel htmlFor="work_time" value="作業時間（分）" />
+			<form onSubmit={submit} className="mt-6 space-y-6">
+				<div>
+					<InputLabel
+						htmlFor="work_time"
+						value={t("profile.updatePreference.workTime")}
+					/>
 
-                    <TextInput
-                        id="work_time"
-                        type="number"
-                        className="mt-1 block w-full"
-                        value={data.work_time}
-                        onChange={(e) =>
-                            setData("work_time", parseInt(e.target.value))
-                        }
-                        required
-                        min="1"
-                    />
+					<TextInput
+						id="work_time"
+						type="number"
+						className="mt-1 block w-full"
+						value={data.work_time}
+						onChange={(e) =>
+							setData("work_time", Number.parseInt(e.target.value))
+						}
+						required
+						min="1"
+					/>
 
-                    <InputError className="mt-2" message={errors.work_time} />
-                </div>
+					<InputError className="mt-2" message={errors.work_time} />
+				</div>
 
-                <div>
-                    <InputLabel htmlFor="break_time" value="休憩時間（分）" />
+				<div>
+					<InputLabel
+						htmlFor="break_time"
+						value={t("profile.updatePreference.breakTime")}
+					/>
 
-                    <TextInput
-                        id="break_time"
-                        type="number"
-                        className="mt-1 block w-full"
-                        value={data.break_time}
-                        onChange={(e) =>
-                            setData("break_time", parseInt(e.target.value))
-                        }
-                        required
-                        min="1"
-                    />
+					<TextInput
+						id="break_time"
+						type="number"
+						className="mt-1 block w-full"
+						value={data.break_time}
+						onChange={(e) =>
+							setData("break_time", Number.parseInt(e.target.value))
+						}
+						required
+						min="1"
+					/>
 
-                    <InputError className="mt-2" message={errors.break_time} />
-                </div>
+					<InputError className="mt-2" message={errors.break_time} />
+				</div>
 
-                <div className="flex items-center gap-4">
-                    <PrimaryButton disabled={processing}>保存</PrimaryButton>
+				<div className="flex items-center gap-4">
+					<PrimaryButton disabled={processing}>
+						{t("common.save")}
+					</PrimaryButton>
 
-                    <Transition
-                        show={recentlySuccessful}
-                        enter="transition ease-in-out"
-                        enterFrom="opacity-0"
-                        leave="transition ease-in-out"
-                        leaveTo="opacity-0"
-                    >
-                        <p className="text-sm text-gray-600 dark:text-gray-400">
-                            保存しました。
-                        </p>
-                    </Transition>
-                </div>
-            </form>
-        </section>
-    );
+					<Transition
+						show={recentlySuccessful}
+						enter="transition ease-in-out"
+						enterFrom="opacity-0"
+						leave="transition ease-in-out"
+						leaveTo="opacity-0"
+					>
+						<p className="text-sm text-gray-600 dark:text-gray-400">
+							{t("common.saved")}
+						</p>
+					</Transition>
+				</div>
+			</form>
+		</section>
+	);
 }
