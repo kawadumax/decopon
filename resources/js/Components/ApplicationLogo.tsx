ceee07d46e3012800013c155c1a1a5a9e27a58ab
kawadumax <kazmax7@gmail.com>
2025-02-21 16:25:28 +0900

    Apply Biome

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -1,6 +1,6 @@
-import { SVGAttributes } from "react";
+import type { SVGAttributes } from "react";
 import { ReactComponent as Logo } from "/public/images/decopon-logo.svg?react";
 
 export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
-    return <Logo {...props} />;
+	return <Logo {...props} />;
 }
