244b4081cf96ec9f328e3ff3f50db68f88d0d9c1
kawadumax <kazmax7@gmail.com>
2025-02-25 20:54:00 +0900

    i18n: Welcome

--- resources/js/Components/LangSwitch.tsx
+++ resources/js/Components/LangSwitch.tsx
@@ -1,23 +1,26 @@
-import { Locale } from "@/types/index.d";
-import { Select } from "@headlessui/react";
 import {
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@radix-ui/react-select";
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/Components/ui/select";
+import { languageAtom } from "@/Lib/atoms";
+import { Locale } from "@/types/index.d";
 import { t } from "i18next";
+import { useAtom } from "jotai";
 
 export const LangSwitch = () => {
-    //TODO: langのAtomを用意して、onValueChangeで切り替える。
+	//TODO: langのAtomを用意して、onValueChangeで切り替える。
+	const [lang, setLang] = useAtom(languageAtom);
 	return (
 		<Select
-			defaultValue={}
-			onValueChange={(e) => {
-				setData("locale", e as Locale);
+			defaultValue={lang}
+			onValueChange={(event: string) => {
+				setLang(event as Locale);
 			}}
 		>
-			<SelectTrigger id="locale">
+			<SelectTrigger id="locale" className="w-fit font-bold h-10">
 				<SelectValue placeholder={t("profile.updatePreference.locale")} />
 			</SelectTrigger>
 			<SelectContent>
