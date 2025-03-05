8ebdc6e8eaf5b4975aa167526923b13b5ac42524
kawadumax <kazmax7@gmail.com>
2025-03-05 14:56:19 +0900

    Update: Apply env to Seeder

--- database/seeders/TimeEntriesTableSeeder.php
+++ database/seeders/TimeEntriesTableSeeder.php
@@ -7,15 +7,17 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class TimeEntriesTableSeeder extends Seeder
+class TimeEntriesTableSeeder extends BaseSeeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+
+        $adminEmail = $this->getAdminEmail();
         //Adminユーザを取得
-        $adminUser = User::where('email', '***REMOVED***')->first();
+        $adminUser = User::where('email', $adminEmail)->first();
         //Adminユーザが存在する場合のみタスクを作成
         if ($adminUser) {
             $this->createTimeEntryForUser($adminUser);
