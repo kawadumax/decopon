6ab46af065a0816e17ea99cbac4ad2df59431a87
kawadumax <kazmax7@gmail.com>
2025-03-24 00:51:35 +0900

    Change: Load .env from root directory

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
@@ -31,3 +31,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Load .env from root directory.
+$app->useEnvironmentPath(realpath(__DIR__ . '/../../'));
+
+return $app;
