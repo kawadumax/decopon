5222909dd4098f8d676fcbc5216ba4e5778f0591
kawadumax <kazmax7@gmail.com>
2025-03-24 13:53:46 +0900

    Disable: Remember me

--- frontend/web/src/ts/pages/auth/Login.tsx
+++ frontend/web/src/ts/pages/auth/Login.tsx
@@ -1,7 +1,6 @@
 import InputLabel from "@/components/InputLabel";
 import PrimaryButton from "@/components/PrimaryButton";
 import TextInput from "@/components/TextInput";
-import { Checkbox } from "@/components/ui/checkbox";
 import { callApi } from "@/lib/apiClient";
 import { useForm } from "@tanstack/react-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -12,7 +11,6 @@ import { DemoCaution } from "./partials/DemoCaution";
 type LoginData = {
   email: string;
   password: string;
-  remember: boolean;
 };
 
 const loginMutationFn = async (loginData: LoginData) => {
@@ -51,7 +49,6 @@ export default function Login({
     defaultValues: {
       email: "",
       password: "",
-      remember: false,
     },
     onSubmit: async ({ value, formApi }) => {
       // mutation.mutate でログインリクエストを実行
@@ -122,25 +119,6 @@ export default function Login({
                 />
                 {/* エラーメッセージ表示例 */}
               </>
-            )}
-          </form.Field>
-        </div>
-
-        <div className="mt-4 block">
-          <form.Field name="remember">
-            {(field) => (
-              <label className="flex items-center" htmlFor={field.name}>
-                <Checkbox
-                  id={field.name}
-                  checked={field.state.value}
-                  onCheckedChange={(checked) =>
-                    field.handleChange(checked as boolean)
-                  }
-                />
-                <span className="ms-2 text-gray-600 text-sm dark:text-gray-400">
-                  {t("auth.login.rememberMe")}
-                </span>
-              </label>
             )}
           </form.Field>
         </div>
