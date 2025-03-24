d1c00d192427dbc18994c82a2a4e8f6927b20cad
kawadumax <kazmax7@gmail.com>
2025-03-25 00:11:22 +0900

    Update: XSRF-TOKEN with interceptor

--- frontend/web/src/ts/lib/axios.ts
+++ frontend/web/src/ts/lib/axios.ts
@@ -14,4 +14,18 @@ const instance = SecuredAxios.create({
   },
 });
 
+instance.interceptors.request.use(
+  async (config) => {
+    // POST や PUT、DELETE など、状態変更系のリクエストの場合のみ
+    if (["post", "put", "delete", "patch"].includes(config.method?.toLowerCase() || "")) {
+      // 例として、クッキーに "XSRF-TOKEN" が含まれているかをチェック
+      if (!document.cookie.includes("XSRF-TOKEN")) {
+        await axios.get("http://localhost:8000/sanctum/csrf-cookie");
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default instance;
