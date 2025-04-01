5d136399f3315ccb4d1e79e38381ff7540d25efe
kawadumax <kazmax7@gmail.com>
2025-04-01 14:42:27 +0900

    Refactor: Remove unused code

--- frontend/web/src/routes/auth/tasks.tsx
+++ frontend/web/src/routes/auth/tasks.tsx
@@ -1,43 +1,8 @@
 import Index from "@/pages/task/Index";
-import type { Task, User } from "@/types";
-import { createFileRoute, useLoaderData } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { t } from "i18next";
 
-// ダミーの非同期関数（実際はAPI呼び出しなどに置き換えます）
-const fetchTasks = async () => {
-  // 例: await fetch('/api/tasks').then(res => res.json());
-  const props = {
-    auth: {
-      user: {
-        id: 1,
-        name: "User",
-      } as User,
-    },
-    tasks: [
-      {
-        id: 1,
-        user_id: 1,
-        completed: false,
-        description: "This is task 1",
-        tags: [],
-        title: "Task 1",
-      },
-    ] as Task[],
-  };
-  return props;
-};
-
 export const Route = createFileRoute("/auth/tasks")({
-  loader: async () => {
-    const { auth, tasks } = await fetchTasks();
-    return {
-      auth,
-      tasks,
-    };
-  },
-  component: () => {
-    const { auth, tasks } = useLoaderData({ from: "/auth/tasks" });
-    return <Index auth={auth} tasks={tasks} />;
-  },
+  component: Index,
   context: () => ({ title: t("task.title") }),
 });
