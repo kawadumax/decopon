6d0c63cafa5b983ea06fbf6166bf09c5bd69780c
kawadumax <kazmax7@gmail.com>
2025-05-21 15:11:48 +0900

    change: alias

--- frontend/web/vite/alias.ts
+++ frontend/web/vite/alias.ts
@@ -3,11 +3,11 @@ import path from "node:path";
 const root = path.resolve(__dirname, "..");
 
 const alias = {
-  "@": path.resolve(root, "./src/ts"),
-  "@components": path.resolve(root, "./src/ts/components"),
-  "@pages": path.resolve(root, "./src/ts/pages"),
+  "@": path.resolve(root, "./src"),
+  "@components": path.resolve(root, "../core/src/scripts/components"),
+  "@pages": path.resolve(root, "../core/src/scripts/pages"),
   "@public": path.resolve(root, "../core/public"),
-  "@core": path.resolve(root, "../core/src/js"),
+  "@core": path.resolve(root, "../core/src"),
 };
 
 export default alias;
