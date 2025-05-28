578e93de1bae9901cf1f9ded206e6821e90b5b32
kawadumax <kazmax7@gmail.com>
2025-05-28 16:10:44 +0900

    refactor

--- frontend/core/.storybook/main.ts
+++ frontend/core/.storybook/main.ts
@@ -1,9 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { mergeConfig } from "vite";
-import svgr from "vite-plugin-svgr";
+
 import alias from "../buildSettings/alias.ts";
+import plugins from "../buildSettings/plugins.ts";
 
 const config: StorybookConfig = {
   stories: ["../stories/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -19,22 +18,9 @@ const config: StorybookConfig = {
   viteFinal: async (config) => {
     return mergeConfig(config, {
       resolve: {
-        alias: alias,
+        alias,
       },
-      plugins: [
-        tailwindcss(),
-        react({
-          babel: {
-            presets: ["jotai/babel/preset"],
-          },
-        }),
-        svgr({
-          svgrOptions: {
-            exportType: "named",
-            ref: true,
-          },
-        }),
-      ],
+      plugins,
       server: {
         port: 6006,
         host: "localhost",
--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,33 +1,10 @@
 import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 import alias from "../core/buildSettings/alias";
-
+import plugins from "../core/buildSettings/plugins";
 export default () => {
   return defineConfig({
-    plugins: [
-      tailwindcss(),
-      TanStackRouterVite({
-        target: "react",
-        autoCodeSplitting: true,
-        routesDirectory: "../core/src/routes",
-        generatedRouteTree: "../core/src/routeTree.gen.ts",
-      }),
-      react({
-        babel: {
-          presets: ["jotai/babel/preset"],
-        },
-      }),
-      svgr({
-        svgrOptions: {
-          exportType: "named",
-          ref: true,
-        },
-      }),
-    ],
+    plugins,
     server: {
       host: "localhost",
       port: 5173,
